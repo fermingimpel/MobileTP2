79733bfbe93f12e4ff9298f8bf63f932afe456a3
fermingimpel <fermingimpel@gmail.com>
2020-10-19 11:46:31 -0300

    Añadido proyecto

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    [SerializeField] GameObject grass;
+    [SerializeField] Transform parent;
+    [SerializeField] int width;
+    [SerializeField] int height;
+    void Start() {
+        for(int i=0;i<width;i++)
+            for(int j = 0; j < height; j++) {
+                Instantiate(grass, new Vector3(i, j, 0), Quaternion.identity, parent);
+            }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
