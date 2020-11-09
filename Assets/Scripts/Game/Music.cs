b8e4a3e11728c69b8c0311576629c5f873c6fe84
fermingimpel <fermingimpel@gmail.com>
2020-11-09 09:41:33 -0300

    Retoques en valores

--- Assets/Scripts/Game/Music.cs
+++ Assets/Scripts/Game/Music.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Music : MonoBehaviour {
     static Music m;
     [SerializeField] AudioSource source;
+    float volume = 0.75f;
     void Awake() {
+        source.volume = volume;
         if (m != null) {
             Destroy(gameObject);
             return;
--- Assets/Scripts/Gameplay/Enemy/EnemyManager.cs
+++ Assets/Scripts/Gameplay/Enemy/EnemyManager.cs
@@ -50,6 +50,11 @@ public class EnemyManager : MonoBehaviour {
     IEnumerator SpawnEnemies() {
         yield return new WaitForSeconds(3.0f);
         while (enemiesCreated < enemiesToCreate) {
+            if (enemiesCreated >= 50)
+                timeToSpawn = 0.8f;
+            if (enemiesCreated >= 100)
+                timeToSpawn = 0.6f;
+
             yield return new WaitForSeconds(timeToSpawn);
             if (canCreateEnemies) {
                 if (enemies[actualEnemy] != null) {
